1e62baa542894f574f0948c9a4d19702507572da
NexON39 <flashxd30501337@gmail.com>
2021-10-25 17:25:38 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int testy, a, b;
+
+void nwd(int liczba1, int liczba2)
+{
+    int reszta=1, dzielnik;
+    while(reszta!=0)
+    {
+        reszta = liczba1%liczba2;
+        if(reszta==0)
+        {
+            dzielnik = liczba2;
+        }
+        else
+        {
+            liczba1 = liczba2;
+            liczba2 = reszta;
+        }
+    }
+    cout << dzielnik << endl;
+}
+
+int main()
+{
+
+    cin >> testy;
+    for(int i=0; i<testy; i++)
+    {
+        cin >> a >> b;
+        nwd(a,b);
+    }
+
+}
